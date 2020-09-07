8383983f4be25651580a5400ed9f544d18f211e4
Şeref Can MUŞTU <mustuserefcan@gmail.com>
2020-09-08 01:27:35 +0300

    blockchain improved

--- src/models/blockchain.ts
+++ src/models/blockchain.ts
@@ -1,19 +1,39 @@
 class Blockchain {
 
-  chain: Block[];
+  private _chain: Block[] = [];
 
   constructor() {
 
   }
 
-  createChain() {
-    this.chain = [
-      this.createGenesisBlock()
-    ];
+  get chain() {
+    return this._chain;
   }
 
   createGenesisBlock(): Block {
     return new Block('data', null);
   }
 
+  createBlock(data: any) {
+    return new Block(data, null);
+  }
+
+  addGenesisBlock() {
+    const genesisBlock = this.createGenesisBlock();
+    this.addBlock(genesisBlock);
+  }
+
+  addBlock(block: Block) {
+    this.chain.push(block);
+  }
+
+  getLatestBlock() {
+    const lastIndex = this.chain.length - 1;
+    return this.chain[lastIndex];
+  }
+
 }
+
+module.exports = {
+  Blockchain: Blockchain
+};
