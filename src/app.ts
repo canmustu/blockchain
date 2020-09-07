ed4638c9fc1cdab16e663c7ff0ba526b2fe0f7f4
Şeref Can MUŞTU <mustuserefcan@gmail.com>
2020-09-08 01:27:50 +0300

    app updated

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 
+import * as BlockController from './controllers/block.controller';
+
 const app = express();
 const port = 3000;
 
-
-
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
+// BlockController(app);
 
 app.listen(port, () => {
   console.log('---------------------------------------------------');
