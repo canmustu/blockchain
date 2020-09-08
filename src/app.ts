539ff6b9cae30a495f4eb4d73bf1c5f01082454d
Şeref Can MUŞTU <mustuserefcan@gmail.com>
2020-09-09 00:23:56 +0300

    app updated

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 
-import * as BlockController from './controllers/block.controller';
+import { Blockchain, Block } from './models';
 
-const app = express();
-const port = 3000;
+const BlockController = require('./controllers/block.controller');
 
+// const app = express();
+// const port = 3000;
+
+/* start api */
+// app.listen(port, () => {
+//   console.log('---------------------------------------------------');
+//   console.log(`Server is listening on port ${port}!`);
+//   console.log('---------------------------------------------------');
+// });
+
+/* set controllers */
 // BlockController(app);
 
-app.listen(port, () => {
-  console.log('---------------------------------------------------');
-  console.log(`Server is listening on port ${port}!`);
-  console.log('---------------------------------------------------');
-});
+/* create genesis block */
+const blockchain = new Blockchain();
+blockchain.createBlock({ amount: 1 });
+blockchain.createBlock({ amount: 2 });
